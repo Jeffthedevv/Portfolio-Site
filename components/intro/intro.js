fe82849439e2ab85d5d2ce1a452b606277433c67
Jeffery Diaz <50490781+Jeffthedevv@users.noreply.github.com>
2022-07-17 17:03:28 -0500

    Adds New Copy for the Welcoming.

--- components/intro/intro.js
+++ components/intro/intro.js
@@ -1,13 +1,41 @@
 
 
 export default function Intro() {
-  
+
   return (
     <div className="intro_wrapper">
       <span className="top_styler_block styler_border"></span>
       <span className="bottom_styler_block styler_border"></span>
+      <div className="content_wrapper">
+        <h1 className="intro_header w-100">Jeffery Diaz</h1>
+        <h3 className="intro_subHeader w-100">Fullstack Developer</h3>
 
-      <h1 className="header">Introduction Component</h1>
+        <h4 className="welcoming">Bienvenidos!</h4>
+        <p>I'm a developer & programmer currently based in Austin, TX. <br/>
+        My expertise is JavsScript but picking up new languages or frameworks isn't a problem. <br />
+        I have extensive knowledge in other languages. I am comfortable developing the frontend or backend as well as 
+        refactoring code to new infrastructures. 
+        </p>
+        
+        <p><span className="style_span">Overview</span> <br />As a fullstack developer it's hard to concatinate what I do on a
+        day to day basis due to the variability,<br />but In the last year I have built 3 middleware applications, using Node. 
+        On another front I am currently heading the development, <br />coordination, and porting to a popular weebsite to a Next JS stack.
+        </p>
+
+        <p>I read the anthology Beautiful Code, by Jon Bentley and it fundementally reaches the root of what I strive for as a programmer, developer, 
+        or *Insert Title Here*<br />
+          - In software, the most beautiful code, the most beautiful functions, and the most beautiful programs are sometimes not there at all.
+        </p>
+
+
+        <div className="quick_links w-100">
+          
+        </div>
+      </div>
+
+
+
+      
     </div>
   )
 }
