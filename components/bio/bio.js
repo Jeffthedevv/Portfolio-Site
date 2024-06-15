b2c6c373e9b8859e1b55e7f687bab75fad640a26
jDiazELK <silentjeff41@hotmail.com>
2024-06-14 19:39:14 -0500

    WIP

--- components/bio/bio.js
+++ components/bio/bio.js
@@ -9,7 +9,18 @@ export default function Bio() {
       <span className={classNames(bio.styler_border, bio.bottom_styler_block)}></span>
 
       <div className={classNames(bio.content_wrapper)}>
-        <h1>Bio Component</h1>
+        <h1>About Me</h1>
+        <div className={""}>
+          <p>My journey in technology started young when my father brough home a Compact PC and I became obsessed with trying to figure out how it worked.
+            I remember waiting for him to leave to work so I could take apart the computer. From molex cables to 40pin connectors, I was mezmoriezed with how those
+            physical lumps of metal and wires could do so much. And before you ask, yes I did get into huge trouble!
+          </p>
+          <p>
+            Over the years I went from learning about the hardware and transitioned to software, where I quickly learned it's what I wanted to do as a career.
+            I have developed a diverse skill set that includes expertise in JavaScript, TypeScript, React, Node.js, PHP, and various databases such as MySQL,
+            MongoDB, and MariaDB. My commitment to continuous learning and staying updated with the latest industry trends has all been for the love of what I do. 
+          </p>
+        </div>
         <div className="quick_links w-100">   
         </div>
       </div>
--- components/network/network.js
+++ components/network/network.js
@@ -5,12 +5,14 @@ const classNames = require("classnames");
 export default function Network() {
   return(
     <div className={classNames(contact.contact_wrapper)}>
+    
       <span className={classNames(contact.top_styler_block)}></span>
       <span className={classNames(contact.bottom_styler_block)}></span>
 
       <div className={classNames(contact.content_wrapper)}>
         <h1>Contact Component</h1>
       </div>
+
     </div>
   )
 }
