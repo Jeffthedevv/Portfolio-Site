d4f59a1fb465dd75074b9d8b558e93cb11532821
JefferyDiaz <silentjeff41@hotmail.com>
2024-08-14 10:52:29 -0500

    Adds Anchor Links to Sections

--- components/intro/intro.js
+++ components/intro/intro.js
@@ -11,15 +11,15 @@ export default function Intro() {
 
       <div className="content_wrapper md-10">
 
-        <div className="welcoming_wrapper">
+        <div className="welcoming_wrapper" id="welcoming">
           <h1 className="intro_header w-100">Jeffery Diaz</h1>
           <h3 className="intro_subHeader w-100">Fullstack Developer</h3>
-          <div className="intro_paragrapth">
+          <div className="intro_paragrapth" id="intro">
             <p>Welcome to my portfolio! I am Jeffery Diaz, a passionate and solution-driven Full Stack Developer, proudly born and raised in New York. As a Hispanic developer, I bring a unique perspective and a rich cultural background to my work. With over 7 years of experience in building consumer-focused websites and applications, I have honed my skills in both front-end and back-end development.</p>
           </div>
         </div>
 
-        <div className="bio_wrapper">
+        <div className="bio_wrapper" id="bio">
 
           <h1 className="w-100 mb-4">About Me</h1>
 
@@ -56,19 +56,19 @@ export default function Intro() {
           </div>
         </div>
 
-        <div className="skills_wrapper">
+        <div className="skills_wrapper" id="skills">
           <div>
             <h1>Skills Component</h1>
           </div>
         </div>
 
-        <div className="lab_wrapper">
+        <div className="lab_wrapper" id="lab">
           <div>
             <h1>Lab Component</h1>
           </div>
         </div>
         
-        <div className="contact_wrapper">
+        <div className="contact_wrapper" id="contact">
           <div>
             <h1>Contact Component</h1>
           </div>
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import Head from 'next/head'
 
-
 import Intro from '../components/intro/intro'
-
+import Link from "next/link"
 
 import { Row, Col, Button, Navbar, Nav } from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -11,21 +10,6 @@ import index from '../styles/index.module.css';
 
 
 export default class Home extends React.Component {
-  constructor(props){
-    super(props)
-    this.setState({ page:'' })
-    
-  }
-
-  UNSAFE_componentWillMount() {
-    this.setState({ page: 'Welcome' })
-  }
-
-  buttonHandler = (event) => {    
-    this.setState({
-      page: event.target.value
-    })
-  }
 
   render() {
     const pageHandler = () => {
@@ -47,31 +31,31 @@ export default class Home extends React.Component {
         <Row className={`${index.wrapper} main-wrapper m-auto`}>
           <Col className={index.nav_wrapper} xs={12} md={2} xxl={1}>
             <Navbar collapseOnSelect expand="md"  id={index.bs_nav} className="navbar navbar-dark position-fixed">
-              <Navbar.Brand>{this.state.page}</Navbar.Brand>
+              <Navbar.Brand></Navbar.Brand>
               <Navbar.Toggle aria-controls="responsive-navbar-nav" />
               <Navbar.Collapse id="responsive-navbar-nav">
                 <Nav className="inner-nav">
-                  <Nav.Link href='#'>
+                  <Link href='#welcoming'>
                     <Button className={`${index.nav_button} active`} value="Welcome">Welcome</Button>  
-                  </Nav.Link>
-                  <Nav.Link href='#'>
+                  </Link>
+                  <Link href='#bio'>
                     <Button className={index.nav_button} value="Bio">Bio</Button>  
-                  </Nav.Link>
-                  <Nav.Link href='#'>
+                  </Link>
+                  <Link href='#skills'>
                     <Button className={index.nav_button} value="Skills">Skills</Button>  
-                  </Nav.Link>
-                  <Nav.Link href='#'>
+                  </Link>
+                  <Link href='#lab'>
                     <Button className={index.nav_button} value="Lab">Lab</Button>
-                  </Nav.Link>
-                  <Nav.Link href='#'>
+                  </Link>
+                  <Link href='#contact'>
                     <Button className={index.nav_button} value="Network">Network</Button>
-                  </Nav.Link>
+                  </Link>
                 </Nav>
               </Navbar.Collapse>
             </Navbar>
           </Col>
           <Col sm={12} md={10} xxl={11} className={index.content_wrapper}>
-            {pageHandler()}
+            <Intro />
           </Col>
         </Row>
       </div>   
